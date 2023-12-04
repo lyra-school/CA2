1036d1d94e5dee18aa5ad8d66d3d8af6f22264d3
lyra-school <s00233718@atu.ie>
2023-12-04 10:20:04 +0000

    Q1d

---
+++ CA2/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    public class Player
+    {
+        private string _name;
+        private string _resultRecord;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string ResultRecord
+        {
+            get { return _resultRecord; }
+        }
+    }
+}
